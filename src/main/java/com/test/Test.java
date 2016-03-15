31cd2f1f30a08c6ed48b2a398308c6cd30ca1ae7
wangyx <965185875@qq.com>
2016-03-15 16:27:58 +0800

    Initial commit

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by Administrator on 2016/3/14.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("helloworld");
+    }
+}
